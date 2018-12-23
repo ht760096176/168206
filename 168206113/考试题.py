31e7ff46d927f646ca035597657dc769e9e32188
ht760096176 <44492027+ht760096176@users.noreply.github.com>
2018-12-23 20:42:36 +0800

    Create 考试题.py

---
+++ 168206113/考试题.py
@@ -0,0 +1,6 @@
+def find():
+     for lier in range(ord('A'),ord('D')+1):
+         if((sum!=(ord('A')))+(lier==(ord('D')))+(lier==(ord('B')))+(lier!=(ord('D')))==1):
+             print(chr(sum))
+ 
+  find()
